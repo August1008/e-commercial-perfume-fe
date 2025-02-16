20f21621d709ed88547965f486a09d67f59eed77
August1008 <ngbinhan1512@gmail.com>
2025-02-16 14:35:34 +0700

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,15 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
-import { HttpClient } from '@angular/common/http';
+import { ShopComponent } from "./features/shop/shop.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ShopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-  baseUrl: string = 'https://localhost:44359/api/'
+export class AppComponent {
   title = 'e-commercial-perfume-fe';
-  private http = inject(HttpClient);
-  products: any[] = [];
-
-  ngOnInit(): void {
-    this.http.get<any>(this.baseUrl + 'Products').subscribe({
-      next: response => {
-        console.log(response);
-        this.products = response.items;
-      },
-      error: error => console.error(error),
-      complete: () => {
-        console.log("complete")
-      }
-      
-    })
-  }
 }
