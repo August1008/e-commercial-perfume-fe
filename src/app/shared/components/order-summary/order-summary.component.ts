10c0cefcee437d8884bb2c9f79d9f2be9819b854
August1008 <ngbinhan1512@gmail.com>
2025-03-11 01:50:36 +0700

    Add order summary component

--- src/app/shared/components/order-summary/order-summary.component.ts
+++ src/app/shared/components/order-summary/order-summary.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormField, MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import { MatInput, MatInputModule } from '@angular/material/input';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-order-summary',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule,
+    RouterLink,
+    MatFormFieldModule,
+    MatLabel,
+    MatInputModule
+  ],
   templateUrl: './order-summary.component.html',
   styleUrl: './order-summary.component.scss'
 })
